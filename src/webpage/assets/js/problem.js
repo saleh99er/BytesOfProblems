4e5c6a06cbccdbfbe7cfa20e42f9ad6703b73fe5
Saleh Hassen <saleh99er@gmail.com>
2020-05-02 18:18:41 -0400

    tried to extract Quill initialization outside of html page, unsuccessful

---
+++ src/webpage/assets/js/problem.js
@@ -0,0 +1,8 @@
+
+// import {Quill} from "https://cdn.quilljs.com/1.3.6/quill.js"
+
+// function generateEditor(){
+//     var editor = new Quill('#editor', { theme: 'snow'});
+// }
+
+// generateEditor()
